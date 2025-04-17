d2fec9d0d4f475309acb4c4f5f6d0933c87a62f7
Shiro219 <rumendez@csumb.edu>
2025-04-17 11:22:21 -0700

    Login/Signup screen with google (No internal function yet)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<LoginPage />}/>
+          <Route path="/2" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </BrowserRouter>
