0e5d39757f4d48ab1ec741b6a83b25df20bdb0cf
Shiro219 <rumendez@csumb.edu>
2025-04-18 14:32:25 -0700

    I fixed the conflicts from part 1.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/1" element={<HomePage />} />
-            <Route path="/" element={<LoginPage />}/>
+          <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />}/>
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </BrowserRouter>
