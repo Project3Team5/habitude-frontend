5154c5bdfbe929f711721dde6bd469b552bb1bfb
Shiro219 <rumendez@csumb.edu>
2025-04-18 14:49:37 -0700

    Not sure why doesn't allow me to push to my main branch

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />}/>
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </BrowserRouter>
